dc4c5cbe81b7efa95fc11e92873d571346e1a0bf
unknown <khangluong2002@gmail.com>
2023-01-04 23:53:03 +0700

    Add story

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,9 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
-      
+      width: {
+        '57%': '57%',
+      },
       backgroundImage: {
         'slider': "url('./img/slide.jpg')",
         'footer-texture': "url('/img/footer-texture.png')",
@@ -13,7 +15,12 @@ module.exports = {
         Karla: ['Karla', 'sans-serif']
       },
       colors: {
-        'light-coffee': '#c89f94'
+        'light-coffee': {
+          '200': '#a25f4b',
+          '400': '#c89f94',
+          
+        },
+        'gray-base':'#1d1f2eb3'
       },
       keyframes: {
         slideDown: {
