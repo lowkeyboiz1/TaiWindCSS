ad800c0b8a885c0292086ec69ae2ced443cf0bf0
unknown <khangluong2002@gmail.com>
2023-01-08 01:00:27 +0700

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,18 +18,28 @@ module.exports = {
         'light-coffee': {
           '200': '#a25f4b',
           '400': '#c89f94',
-          
+
         },
-        'gray-base':'#1d1f2eb3'
+        'gray-base': '#1d1f2eb3'
       },
       keyframes: {
+        slideUp: {
+          '0%': {
+            transform: 'translate(-50%, 20px)',
+          },
+          '100%': { transform: 'translate(-50%, 0px)'},
+        },
         slideDown: {
-          '0%': { transform: 'translateY(-100%)' },
-          '100%': { transform: 'translateY(0)' },
-        }
+          '0%': {
+            transform: 'translateY(-8px)',
+          },
+          '100%': { transform: 'translateY(0)'},
+        },
+
       },
       animation: {
-        slideDown: 'slideDown 0.4s ease-in-out',
+        slideUp: 'slideUp 0.2s ease-in-out',
+        slideDown: 'slideDown 0.2s ease-in-out',
       }
     },
   },
